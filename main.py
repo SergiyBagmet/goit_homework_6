7e3fed4a3afe95f1f93e46a09b73e2766662dca5
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-07-06 12:38:39 +0300

    group_func
    - code in tree(if .is_file) to 3 funcs
    +exept archive
    +re.sub in normalize
    +main()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import shutil
-
+import re
 # известние файли для сортировки
 FILE_EXTENSIONS = {
     'images': {'.jpeg', '.png', '.jpg', '.svg'},
@@ -9,7 +9,7 @@ FILE_EXTENSIONS = {
     'audio': {'.mp3', '.ogg', '.wav', '.amr'},
     'archives': {'.zip', '.gz', '.tar'}
 }
-
+my_extens = set() # сет для записи всех имеющихся расширений в папке
 # константи для переименования файлов
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
@@ -27,18 +27,43 @@ def normalize(name : str) -> str :
     транлитерация кирилици
     замена всего кроме латинского алфавита и чисел на _
     """
-    if name.isalnum() :              
-        return name.translate(TRANS)
-    else:
-        new_name = ""
-        for ch in name:           
-            if ch.isalnum():
-                new_name += ch.translate(TRANS)
-            else :
-                new_name += "_"    
-        return new_name
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)  
+    return t_name  
 
+def new_path_name(ex: Path) -> Path:
+    """
+    переименование по транслиту + "_"
+    возврат нового пути(../файл.суф)
+    """
+    new_name = normalize(ex.stem) + ex.suffix # новое имя + суфикс
+    new_ex = ex.rename(ex.parent / new_name) # переименование файлов + присваеваем новий путь
+    return new_ex
 
+def get_new_folder_name(ex: Path) -> str:
+    """
+    получаем имя для новой папки(по расширению файла) 
+    соответствующей файловой группе 
+    """
+    name_dir = "" 
+    for key,val in FILE_EXTENSIONS.items():
+        if ex.suffix in val : 
+            name_dir = key
+            break
+    if not name_dir: # если нет совпадений по константе
+        name_dir = "others"  
+
+    return name_dir     
+
+def replace_file_new_dir(ex: Path, name_dir: str) :
+    """
+    сознаем новую папку если такой нет
+    перенос файла -> возврат нового пути 
+    """
+    new_dir_path = main_path / name_dir # путь к новой папке для ее создание и переноса файлов
+    new_dir_path.mkdir(exist_ok=True) # создаем новую папку если такой нет 
+    new_ex_path = ex.replace(new_dir_path / ex.name) # перенос файлов в папки по категориям
+    return new_ex_path
 
 def  extract_archive(archive_path: Path, del_archive=True) :
     """
@@ -53,8 +78,9 @@ def  extract_archive(archive_path: Path, del_archive=True) :
             archive_path.unlink()
     except ValueError:
         print(f"Не удалось разпаковать архив : {archive_path.name}")
+    except shutil.ReadError:
+        print(f"Архив - {archive_path.stem} не распакован\tнеизвестное расширение({archive_path.suffix})")
   
-
 
 def input_path()-> Path:
     """
@@ -70,7 +96,6 @@ def input_path()-> Path:
 
 
 
-
 def directory_tree (path: Path,  my_dict_files=None) :
     """
     основной рекурсивний проход 
@@ -79,35 +104,24 @@ def directory_tree (path: Path,  my_dict_files=None) :
     for item in path.iterdir() :
         if item.is_file():
             
-            new_name = normalize(item.stem) + item.suffix # новое имя + суфикс
-            item = item.rename(item.parent / new_name) # переименование файлов + присваеваем новий путь
+            item = new_path_name(item) # переименование файла
 
-            name_dir = "" # получаем имя для новой папки соответствующей файловой группе 
-            for key,val in FILE_EXTENSIONS.items():
-                if item.suffix in val : #
-                    name_dir = key
-                    break
-            if not name_dir: # если нет совпадений по константе
-                name_dir = "others"    
+            name_dir = get_new_folder_name(item) # имя для новой папки
+
+            item = replace_file_new_dir(item, name_dir) # создание папок + перемещение файлов
+
+            if name_dir == "archives": # если категория ахиви(файл можно разпаковать TODO list from shatil) 
+                extract_archive(item) # разпаковка + по умолчанию флаг на удаление 
+
             
-            # заполнение сета расширений my_extens
-            # + заполнение my_dict_files-> категория : [файли]
-            my_extens.add(item.suffix)
+            # get_data: set of ex +  my_dict_files-> категория : [файли] TODO отдельная функция?
+            my_extens.add(item.suffix)  # заполнение сета расширений 
             if  my_dict_files is None :  my_dict_files = {} #создаем словарь на первом заходе 
             if name_dir not in my_dict_files : # если нет ключа создаем ключ:спиок
                 my_dict_files[name_dir] = [item.stem]
             else:
                 my_dict_files[name_dir].append(item.stem)         
 
-            # создание папок + перемещение файлов + работа с архивами 
-            new_dir_path = main_path / name_dir # путь к новой папке для ее создание и переноса файлов
-            new_dir_path.mkdir(exist_ok=True) # создаем новую папку если такой нет 
-            item = item.replace(new_dir_path / item.name) # перенос файлов в папки по категориям
-
-            if name_dir == "archives": # если категория ахиви(файл можно разпаковать) 
-                extract_archive(item) # разпаковка + по умолчанию флаг на удаление 
-            
-            
 
         elif item.is_dir() and (item.name not in FILE_EXTENSIONS) : # проверка на папку и она не из наших ключей
 
@@ -118,23 +132,15 @@ def directory_tree (path: Path,  my_dict_files=None) :
 
     return  my_dict_files
 
-
-
-if __name__ == "__main__" :
-
+def main():
     # получаем сет всех известих(скрипту) расширений из константного словаря
     all_extens = set()
     for ext in FILE_EXTENSIONS.values():
         all_extens.update(ext)
 
     # my_dict_files = {} # словарь для записи -  категория : [файли]
-    my_extens = set() # сет для записи всех имеющихся расширений в папке
-
-    path = input_path()
-    main_path = path # путь к нашей папке для создания нових
-    my_dict_files = directory_tree(path)
     
-
+    my_dict_files = directory_tree(path) # визов рекурсивного прохода
     
     print("\nсписок файлов в сортированной дериктории  по категориям :\n")
     for key, val in my_dict_files.items() :
@@ -148,3 +154,10 @@ if __name__ == "__main__" :
     print("\nПеречень всех расширений не известних данному скрипту :\n")
     n_know_extens = my_extens.difference(know_extens)
     print("\t".join(n_know_extens))
+
+
+if __name__ == "__main__" :
+    path = input_path()
+    main_path = path # путь к нашей папке для создания нових
+    main()
+    
